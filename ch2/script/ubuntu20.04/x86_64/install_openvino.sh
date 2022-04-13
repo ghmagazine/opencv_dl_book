0c53ff1063c0285c5d9e1daadf80f6a1b1136b73
atinfinity <dandelion1124@gmail.com>
2022-04-14 00:40:04 +0900

    add setup script for Ubuntu 20.04(x86_64)

---
+++ ch2/script/ubuntu20.04/x86_64/install_openvino.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+OPENVINO_VERSION=2022.1.0
+
+# please refer to https://docs.openvino.ai/2022.1/openvino_docs_install_guides_installing_openvino_apt.html
+
+# Install the GPG key for the repository
+wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+echo "deb https://apt.repos.intel.com/openvino/2022 focal main" | sudo tee /etc/apt/sources.list.d/intel-openvino-2022.list
+sudo apt update
+
+# Install OpenVINO Runtime Using the APT Package Manager
+sudo apt -y install openvino-${OPENVINO_VERSION}
+sudo ln -s /opt/intel/openvino_2022 /opt/intel/openvino
+echo "source /opt/intel/openvino/setupvars.sh" >> $HOME/.bashrc
+source $HOME/.bashrc
+
